7b7056d82df39f338d3873c7fe52b6fd4a25343b
GarryG6 <ingold333@mail.ru>
2018-08-05 22:58:59 +0800

    Добавлены комментарии.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Получаем GET домашнюю страницу. */
+router.get('/', function(req, res, next) {  // Здесь маршрутизатор будет искать URL
+  res.render('index', { title: 'Express' });  // Содержимое контроллера
 });
 
 module.exports = router;
