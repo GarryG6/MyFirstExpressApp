4a05569008321562dcad978320bcaff29a1e48fc
GarryG6 <ingold333@mail.ru>
2018-08-06 21:29:36 +0800

    + Bootstrap
    + Bootstrap

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,4 @@
-/ Получаем GET домашнюю страницу
+// Получаем GET домашнюю страницу
 module.exports.index = function (req, res) {    // Создаем метод для импорта index
     res.render('index', {title: 'Express'});    // Включаем код контроллера для домашней страницы
 };
