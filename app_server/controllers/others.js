8914ad519e905a63a357a41f8b727dc321e52bca
GarryG6 <ingold333@mail.ru>
2018-08-12 14:31:37 +0800

    + New home page style

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,4 @@
 /* Получить (GET) страницу About (О нас) */
-module.exports.about = function (req,res) {
-    res.render('index', {title: 'About'});
-}
+module.exports.about = function(req, res) {
+  res.render("index", { title: "About" });
+};
