ea6cdab7316ab263c2fad7db409411d16efe7e85
GarryG6 <ingold333@mail.ru>
2018-08-13 00:19:09 +0800

    + New location info page

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,14 +1,14 @@
 /* Получить (GET) домашнюю страницу*/
-module.exports.homelist = function (req, res) {
-    res.render('location-list', {title: 'Home'});
+module.exports.homelist = function(req, res) {
+  res.render("location-list", { title: "Home" });
 };
 
 /* Получить (GET) страницу с информацией о местоположениях */
-module.exports.locationInfo = function (req, res) {
-    res.render('index', {title: 'Location Info'});
+module.exports.locationInfo = function(req, res) {
+  res.render("location-info", { title: "Location Info" });
 };
 
 /* Получить (GET) страницу с добавлением отзыва*/
-module.exports.addReview = function (req,res) {
-    res.render('index',{title:'Add Review'});
+module.exports.addReview = function(req, res) {
+  res.render("index", { title: "Add Review" });
 };
