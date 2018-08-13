532b65a6fce4287ee56358df8435a9dbaed42635
GarryG6 <ingold333@mail.ru>
2018-08-13 18:49:02 +0800

    + New about page

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,4 @@
 /* Получить (GET) страницу About (О нас) */
 module.exports.about = function(req, res) {
-  res.render("index", { title: "About" });
+  res.render("generic-text", { title: "About" });
 };
